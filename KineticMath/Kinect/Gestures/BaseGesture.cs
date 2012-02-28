57472ab8d99c41c5bcee8a71875fad928d8592d7
Kingston <git@kingstontam.com>
2012-02-27 19:03:48 -0800

    Increasing resolution

---
+++ KineticMath/Kinect/Gestures/BaseGesture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+using Coding4Fun.Kinect.Wpf;
+using KineticMath.SubControls;
+
+namespace KineticMath.Kinect.Gestures
+{
+    public abstract class BaseGesture : IGesture
+    {
+        public abstract void ProcessSkeleton(Microsoft.Kinect.Skeleton skeleton);
+
+        /// <summary>
+        /// Scales a joint to a fixed 640 by 480 plane
+        /// </summary>
+        /// <param name="skel"></param>
+        /// <param name="joint"></param>
+        /// <returns></returns>
+        private Joint GetScaledJoint(Skeleton skel, JointType joint)
+        {
+            return skel.Joints[JointType.HandLeft].ScaleTo(640, 480, KinectSkeleton.k_xMaxJointScale, KinectSkeleton.k_yMaxJointScale);
+        }
+
+        private Joint GetJointScaledToScreen(Skeleton skel, JointType joint)
+        {
+            // TODO: Don't hard code values
+            return skel.Joints[JointType.HandLeft].ScaleTo(1200, 675, KinectSkeleton.k_xMaxJointScale, KinectSkeleton.k_yMaxJointScale);
+        }
+    }
+}
